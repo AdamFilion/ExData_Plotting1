9daf75c15cfe07e60e0fc5dc3dfa2635399f4b3a
unknown <afilion@AH-AFILION.dhcp.mathworks.com>
2014-08-10 17:29:56 -0400

    Updated Files

--- plot1.R
+++ plot1.R
@@ -1,30 +1,29 @@
-plot1 <- function(){
-    # data set start date is Dec 16, 2006 at 17:24:00
-    # only want data from Feb 1, 2007 00:00:00 to Feb 2, 2007 24:00:00
-    
-    # read in first line for start information
-    startdate = read.csv("household_power_consumption.txt",sep=";",nrow=1)
-    # convert to date/time information
-    startdate$datetime = strptime( paste(startdate$Date,startdate$Time),"%d/%m/%Y %H:%M:%S")
-    # start date for data analyzed, plus 1 minute since subtracting
-    adate = strptime("01/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
-    # end date for data analyzed, plus 1 minute since subtracting
-    bdate = strptime("03/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
-    # number of rows in data file to skip
-    numskip = as.double(adate-startdate$datetime)*24*60 
-    # number of rows to read
-    numrow = as.double(bdate-adate)*24*60 
-    # read in data to be analyzed
-    cnames = read.csv("household_power_consumption.txt",sep=";",nrow=1,header=FALSE,stringsAsFactors=FALSE)
-    cnames = as.vector(cnames,"character")
-    hpc = read.csv("household_power_consumption.txt",sep=";",skip=numskip,nrow=numrow,na.strings="?",header=FALSE,col.names=cnames)
-    hpc$datetime = strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
-    
-    # create desired plot
-    hist(hpc$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
-    
-    # save to png
-    png(filename="plot1.png",width=480,height=480)
-    hist(hpc$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
-    dev.off()
-}
+
+# data set start date is Dec 16, 2006 at 17:24:00
+# only want data from Feb 1, 2007 00:00:00 to Feb 2, 2007 24:00:00
+
+# read in first line for start information
+startdate = read.csv("household_power_consumption.txt",sep=";",nrow=1)
+# convert to date/time information
+startdate$datetime = strptime( paste(startdate$Date,startdate$Time),"%d/%m/%Y %H:%M:%S")
+# start date for data analyzed, plus 1 minute since subtracting
+adate = strptime("01/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
+# end date for data analyzed, plus 1 minute since subtracting
+bdate = strptime("03/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
+# number of rows in data file to skip
+numskip = as.double(adate-startdate$datetime)*24*60 
+# number of rows to read
+numrow = as.double(bdate-adate)*24*60 
+# read in data to be analyzed
+cnames = read.csv("household_power_consumption.txt",sep=";",nrow=1,header=FALSE,stringsAsFactors=FALSE)
+cnames = as.vector(cnames,"character")
+hpc = read.csv("household_power_consumption.txt",sep=";",skip=numskip,nrow=numrow,na.strings="?",header=FALSE,col.names=cnames)
+hpc$datetime = strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+
+# create desired plot
+hist(hpc$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+
+# save to png
+png(filename="plot1.png",width=480,height=480)
+hist(hpc$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,37 +1,36 @@
-plot3 <- function(){
-    # data set start date is Dec 16, 2006 at 17:24:00
-    # only want data from Feb 1, 2007 00:00:00 to Feb 2, 2007 24:00:00
-    
-    # read in first line for start information
-    startdate = read.csv("household_power_consumption.txt",sep=";",nrow=1)
-    # convert to date/time information
-    startdate$datetime = strptime( paste(startdate$Date,startdate$Time),"%d/%m/%Y %H:%M:%S")
-    # start date for data analyzed, plus 1 minute since subtracting
-    adate = strptime("01/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
-    # end date for data analyzed, plus 1 minute since subtracting
-    bdate = strptime("03/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
-    # number of rows in data file to skip
-    numskip = as.double(adate-startdate$datetime)*24*60 
-    # number of rows to read
-    numrow = as.double(bdate-adate)*24*60 
-    # read in data to be analyzed
-    cnames = read.csv("household_power_consumption.txt",sep=";",nrow=1,header=FALSE,stringsAsFactors=FALSE)
-    cnames = as.vector(cnames,"character")
-    hpc = read.csv("household_power_consumption.txt",sep=";",skip=numskip,nrow=numrow,na.strings="?",header=FALSE,col.names=cnames)
-    hpc$datetime = strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
-    
-    # create desired plot
-    plot(hpc$datetime,hpc$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
-    lines(hpc$datetime,hpc$Sub_metering_2,type="l",col="red")
-    lines(hpc$datetime,hpc$Sub_metering_3,type="l",col="blue")
-    legend("topright",c(colnames(hpc)[7],colnames(hpc)[8],colnames(hpc)[9]),
-           lty=c(1,1,1),lwd=c(2,2,2),col=c("black","red","blue"))
-    # save to png
-    png(filename="plot3.png",width=480,height=480)
-    plot(hpc$datetime,hpc$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
-    lines(hpc$datetime,hpc$Sub_metering_2,type="l",col="red")
-    lines(hpc$datetime,hpc$Sub_metering_3,type="l",col="blue")
-    legend("topright",c(colnames(hpc)[7],colnames(hpc)[8],colnames(hpc)[9]),
-           lty=c(1,1,1),lwd=c(2,2,2),col=c("black","red","blue"))
-    dev.off()
-}
+
+# data set start date is Dec 16, 2006 at 17:24:00
+# only want data from Feb 1, 2007 00:00:00 to Feb 2, 2007 24:00:00
+
+# read in first line for start information
+startdate = read.csv("household_power_consumption.txt",sep=";",nrow=1)
+# convert to date/time information
+startdate$datetime = strptime( paste(startdate$Date,startdate$Time),"%d/%m/%Y %H:%M:%S")
+# start date for data analyzed, plus 1 minute since subtracting
+adate = strptime("01/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
+# end date for data analyzed, plus 1 minute since subtracting
+bdate = strptime("03/02/2007 00:01:00","%d/%m/%Y %H:%M:%S") 
+# number of rows in data file to skip
+numskip = as.double(adate-startdate$datetime)*24*60 
+# number of rows to read
+numrow = as.double(bdate-adate)*24*60 
+# read in data to be analyzed
+cnames = read.csv("household_power_consumption.txt",sep=";",nrow=1,header=FALSE,stringsAsFactors=FALSE)
+cnames = as.vector(cnames,"character")
+hpc = read.csv("household_power_consumption.txt",sep=";",skip=numskip,nrow=numrow,na.strings="?",header=FALSE,col.names=cnames)
+hpc$datetime = strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+
+# create desired plot
+plot(hpc$datetime,hpc$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(hpc$datetime,hpc$Sub_metering_2,type="l",col="red")
+lines(hpc$datetime,hpc$Sub_metering_3,type="l",col="blue")
+legend("topright",c(colnames(hpc)[7],colnames(hpc)[8],colnames(hpc)[9]),
+       lty=c(1,1,1),lwd=c(2,2,2),col=c("black","red","blue"))
+# save to png
+png(filename="plot3.png",width=480,height=480)
+plot(hpc$datetime,hpc$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
+lines(hpc$datetime,hpc$Sub_metering_2,type="l",col="red")
+lines(hpc$datetime,hpc$Sub_metering_3,type="l",col="blue")
+legend("topright",c(colnames(hpc)[7],colnames(hpc)[8],colnames(hpc)[9]),
+       lty=c(1,1,1),lwd=c(2,2,2),col=c("black","red","blue"))
+dev.off()
